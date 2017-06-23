8f3e572a68c475f41d13cd8b8f388d0f77fda2dd
kgigova <kristiana.gigova@gmail.com>
2017-06-23 21:22:07 +0300

    Adding useless classs...

---
+++ BookStoreCatalog/Utilities/MailProcessor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Net.Mail;
+
+namespace BookStoreCatalog.Utilities
+{
+    public class MailProcessor
+    {
+        private string host = "smtp.gmail.com";
+        private int port = 587;
+        private string username = "";
+        private string password = "";
+        private MailMessage message;
+
+        public MailProcessor()
+        {
+            this.message = new MailMessage();
+
+            this.message.From = new MailAddress(username, username, System.Text.Encoding.UTF8);
+            this.message.SubjectEncoding = System.Text.Encoding.UTF8;
+            this.message.BodyEncoding = System.Text.Encoding.UTF8;
+            this.message.IsBodyHtml = true;
+            this.message.Priority = MailPriority.High;
+        }
+
+        public MailMessage Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public void SendMail()
+        {
+            /*
+            MailMessage mail = new MailMessage();
+            mail.To.Add("to gmail address");
+            mail.From = new MailAddress("from gmail address", "Email head", System.Text.Encoding.UTF8);
+            mail.Subject = "This mail is send from asp.net application";
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = "This is Email Body Text";
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+             */
+            System.Net.NetworkCredential mycredentials = new System.Net.NetworkCredential(username, password);
+
+            SmtpClient client = new SmtpClient();
+            client.Credentials = mycredentials;
+            client.Port = port;
+            client.Host = host;
+            client.EnableSsl = true;
+            try
+            {
+
+                client.Send(this.message);
+            }
+            catch (Exception e)
+            {
+                String msg = e.Message;
+            }
+        }
+    }
+}
